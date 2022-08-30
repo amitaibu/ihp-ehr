894273744c7560e0f0332c82320c074f97d178e3
Amitai Burstein <amitai@gizra.com>
2022-08-30 15:53:01 +0300

    Rename tables

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -14,28 +14,28 @@ CREATE TABLE person_child (
 CREATE TABLE users (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL
 );
-CREATE TABLE observation__child_height (
+CREATE TABLE observation_child_height (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
     person_child_id UUID NOT NULL
 );
-CREATE INDEX observation__child_height_created_at_index ON observation__child_height (created_at);
+CREATE INDEX observation_child_height_created_at_index ON observation_child_height (created_at);
 CREATE TABLE person_adult (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL
 );
-CREATE INDEX observation__child_height_person_child_id_index ON observation__child_height (person_child_id);
-CREATE TABLE relation__person_child__person_adult (
+CREATE INDEX observation_child_height_person_child_id_index ON observation_child_height (person_child_id);
+CREATE TABLE relation_person_child_person_adult (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
     person_child_id UUID NOT NULL,
     person_adult_id UUID NOT NULL,
     adult_relation_to_child adult_relation_to_child NOT NULL
 );
-CREATE INDEX relation__person_child__person_adult_created_at_index ON relation__person_child__person_adult (created_at);
-CREATE INDEX relation__person_child__person_adult_person_child_id_index ON relation__person_child__person_adult (person_child_id);
-CREATE INDEX relation__person_child__person_adult_person_adult_id_index ON relation__person_child__person_adult (person_adult_id);
-ALTER TABLE observation__child_height ADD CONSTRAINT observation__child_height_ref_person_child_id FOREIGN KEY (person_child_id) REFERENCES person_child (id) ON DELETE NO ACTION;
-ALTER TABLE relation__person_child__person_adult ADD CONSTRAINT relation__person_child__person_adult_ref_person_adult_id FOREIGN KEY (person_adult_id) REFERENCES person_adult (id) ON DELETE NO ACTION;
-ALTER TABLE relation__person_child__person_adult ADD CONSTRAINT relation__person_child__person_adult_ref_person_child_id FOREIGN KEY (person_child_id) REFERENCES person_child (id) ON DELETE NO ACTION;
+CREATE INDEX relation_person_child_person_adult_created_at_index ON relation_person_child_person_adult (created_at);
+CREATE INDEX relation_person_child_person_adult_person_child_id_index ON relation_person_child_person_adult (person_child_id);
+CREATE INDEX relation_person_child_person_adult_person_adult_id_index ON relation_person_child_person_adult (person_adult_id);
+ALTER TABLE observation_child_height ADD CONSTRAINT observation_child_height_ref_person_child_id FOREIGN KEY (person_child_id) REFERENCES person_child (id) ON DELETE NO ACTION;
+ALTER TABLE relation_person_child_person_adult ADD CONSTRAINT relation_person_child_person_adult_ref_person_adult_id FOREIGN KEY (person_adult_id) REFERENCES person_adult (id) ON DELETE NO ACTION;
+ALTER TABLE relation_person_child_person_adult ADD CONSTRAINT relation_person_child_person_adult_ref_person_child_id FOREIGN KEY (person_child_id) REFERENCES person_child (id) ON DELETE NO ACTION;
